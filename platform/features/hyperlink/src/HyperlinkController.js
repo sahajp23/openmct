7b7fb47cde8f1a1c08bd51b7c07005b60103f555
sahajp23 <Sahaj@Venkatas-MBP.ndc.nasa.gov>
2017-07-17 16:24:14 -0700

    Added documentation to hyperlink domain type object

--- platform/features/hyperlink/src/HyperlinkController.js
+++ platform/features/hyperlink/src/HyperlinkController.js
@@ -1,14 +1,19 @@
+/**
+ * This bundle adds the Hyperlink object type, which can be used to add hyperlinks as a domain Object type
+ and into display Layouts as either a button or link that can be chosen to open in either the same tab or
+ create a new tab to open the link in
+ * @namespace platform/features/hyperlink
+ */
 define(
     [],
     function () {
         function HyperlinkController($scope) {
           this.$scope = $scope
-          console.log(this.$scope.domainObject.getModel())
         }
 
-        /*Function used to return if the property openNewTab should be true and a new tab should be opened
-        false and the hyperlink should open in the same window
-        */
+        /**Function to analyze the location in which to open the hyperlink
+        @returns true if the hyperlink is chosen to open in a different tab, false if the same tab
+        **/
         HyperlinkController.prototype.openNewTab = function()
         {
           if(this.$scope.domainObject.getModel().openNewTab[0]=="thisTab"){
@@ -18,8 +23,9 @@ define(
               return true
             }
         }
-        /*Returns true if the property of the hyperlink should create a button, false if a link is desired
-        */
+        /**Function to specify the format in which the hyperlink should be created
+        @returns true if the hyperlink is chosen to be created as a button, false if a link
+        **/
         HyperlinkController.prototype.isButton = function()
         {
           if(this.$scope.domainObject.getModel().displayFormat[0]=="link"){
